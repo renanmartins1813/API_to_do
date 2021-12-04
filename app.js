b82b1c01d3a2702f3336816989c005c486df74d6
renanmartins1813 <renanmartins1813@github.com>
2021-12-04 03:02:00 -0500

    hj é dia de usar droga?

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 //jshint eversion:6
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const ejs = require('ejs');
+const moment = require('moment');
 const app = express();
+const port = 4040;
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
-    res.send('Fucc')
+    const todayM = moment();
+    const currentyDay = todayM.day();
+
+    if(currentyDay === 6 || currentyDay === 0){
+        res.send(`DIA: ${currentyDay} FODENDO DIA DE USAR DROGA!!!`);
+    }
+    else{
+        res.send(`Dia: ${currentyDay} :( dia de ficar tristo`)
+    }
 })
 
-app.listen(4040, ()=>{
-    console.log("Server started on port 4040");
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`);
 })
