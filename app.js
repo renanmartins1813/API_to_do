e22901504a286cb06352567cf2715ee939e3e573
renanmartins1813 <renanmartins1813@github.com>
2021-12-04 03:36:40 -0500

    iniciando com o EJS

--- app.js
+++ app.js
@@ -11,13 +11,43 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res)=>{
     const todayM = moment();
     const currentyDay = todayM.day();
+    let day = '';
+    
+    switch (currentyDay) {
+        case 0:
+            day = 'Sunday';
+        break;
 
-    if(currentyDay === 6 || currentyDay === 0){
-        res.send(`DIA: ${currentyDay} FODENDO DIA DE USAR DROGA!!!`);
+        case 1:
+            day = 'Moonday';
+        break;
+
+        case 2:
+            day = 'Tuesday';
+        break;
+
+        case 3:
+            day = 'Wednesday';
+        break;
+
+        case 4:
+            day = 'Thursday';
+        break;
+
+        case 5:
+            day = 'Friday';
+        break;
+
+        case 6:
+            day = 'Saturday';
+        break;
+    
+        default:
+            console.log(`Error: current day value is ${currentyDay}`);
+        break;
     }
-    else{
-        res.send(`Dia: ${currentyDay} :( dia de ficar tristo`)
-    }
+
+    res.render('list', {day_ejs: day})
 })
 
 app.listen(port, ()=>{
