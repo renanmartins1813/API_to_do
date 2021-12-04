56438e9534131db8bec85f5c6c9b01535c4b86c4
renanmartins1813 <renanmartins1813@github.com>
2021-12-04 16:52:14 -0500

    a form to /post

--- app.js
+++ app.js
@@ -2,52 +2,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const moment = require('moment');
 const app = express();
 const port = 4040;
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res)=>{
-    const todayM = moment();
-    const currentyDay = todayM.day();
-    let day = '';
-    
-    switch (currentyDay) {
-        case 0:
-            day = 'Sunday';
-        break;
+    const today = new Date();
 
-        case 1:
-            day = 'Moonday';
-        break;
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
 
-        case 2:
-            day = 'Tuesday';
-        break;
-
-        case 3:
-            day = 'Wednesday';
-        break;
-
-        case 4:
-            day = 'Thursday';
-        break;
-
-        case 5:
-            day = 'Friday';
-        break;
-
-        case 6:
-            day = 'Saturday';
-        break;
-    
-        default:
-            console.log(`Error: current day value is ${currentyDay}`);
-        break;
-    }
+    const day = today.toLocaleDateString('en-US', options)
 
     res.render('list', {day_ejs: day})
+});
+
+app.post('/', (req, res)=>{
+    const item = req.body.newItem;
+    console.log(item);
 })
 
 app.listen(port, ()=>{
