07be3c35d37120344db6d3d48d4c5fee1f953f3d
renanmartins1813 <renanmartins1813@github.com>
2021-12-05 12:23:43 -0500

    css

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const port = 4040;
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+let items = ['frist', 'second', 'third'];
 
 app.get('/', (req, res)=>{
     const today = new Date();
@@ -18,16 +21,18 @@ app.get('/', (req, res)=>{
         month: 'long'
     };
 
-    const day = today.toLocaleDateString('en-US', options)
+    const day = today.toLocaleDateString('en-US', options);
 
-    res.render('list', {day_ejs: day})
+    res.render('list', {title_ejs: day, newListItems: items});
 });
 
 app.post('/', (req, res)=>{
     const item = req.body.newItem;
-    console.log(item);
-})
+    items.push(item);
+    
+    res.redirect('/');
+});
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`);
-})
+});
