8eb662ec08892e99573158e7a2cc65698a327c9a
renanmartins1813 <renanmartins1813@github.com>
2021-12-06 05:37:32 -0500

    criando uma lista default para ser adicionada caso o DB esteja vazio

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js')
 const app = express();
 const port = 4040;
@@ -11,12 +12,51 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-const items = ['frist', 'second', 'third'];
-const work_items = [];
+// const items = ['frist', 'second', 'third'];
+// const work_items = [];
+
+mongoose.connect('mongodb://martins:root1432@localhost:27018/al_to_doDB?authSource=admin');
+
+const item_schema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, `a name to a new 'item' is required, check your inputs`]
+    }
+});
+
+const Item = mongoose.model('Item', item_schema);
+
+const to_do_01 = new Item({
+    name: `LF for a job`
+});
+
+const to_do_02 = new Item({
+    name: `procuro emprego jr.`
+});
+
+const to_do_03 = new Item({
+    name: `pessoa recrutadora me nota pls`
+});
+
+const default_items = [to_do_01, to_do_02, to_do_03];
 
 app.get('/', (req, res)=>{
     const day = date.getDate();
-    res.render('list', {title_ejs: day, newListItems: items});
+    const items = [];
+    Item.find({},(err, item)=>{
+        if(err){
+            console.log(err);
+        }
+        else if(item.length === 0){
+            Item.insertMany(default_items, err=>{
+                err ? console.log(err) : console.log(`default_items saved`);
+            });
+            res.redirect('/');
+        }
+        else{
+            res.render('list', {title_ejs: day, newListItems: item});
+        }
+    });
 });
 
 app.post('/', (req, res)=>{
